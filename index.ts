f7e47b33d29482bb6a5425d18ab4509236bb7d32
Anwesh43 <anweshthecool0@gmail.com>
2020-08-15 16:22:06 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
+import { ContextReplacementPlugin } from "webpack"
+
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
-const scGap : number = 0.02 
+const parts : number = 4 
+const lines : number = 6 
+const scGap : number = 0.02 / parts  
 const strokeFactor : number = 90 
 const sizeFactor : number = 1.8 
 const gapFactor : number = 3.8 
@@ -23,3 +27,47 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 } 
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawRotLadder(context : CanvasRenderingContext2D, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(sf, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const gap : number = Math.min(w, h) / gapFactor 
+        const hGap : number = size / (lines + 2)
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(Math.PI / 2 * sf4)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.translate(-gap * 0.5 * sf2, -size / 2)
+            DrawingUtil.drawLine(context, 0, 0, 0, size * sf1)
+            context.restore()
+        }
+        for (var j = 0; j < 6; j++) {
+            context.save()
+            context.translate(0, hGap + hGap * j)
+            DrawingUtil.drawLine(context, -gap * 0.5 * sf3, 0, gap * 0.5 * sf3, 0)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static  drawRLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawRotLadder(context, scale)
+    }
+}
